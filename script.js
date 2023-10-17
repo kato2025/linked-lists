6fde0a4c6e4e99bcd64861b18885e67e750bca0b
Dr. Kato Samuel Namuene <kato.namuene@ubuea.cm>
2023-10-17 13:23:54 +0100

    Add script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+//Create Node class
 class Node {
     constructor(value) {
         this.value = value;
         this.nextNode = null;
     }
 }
-
+//Create LonkedList class
 class LinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
         this.size = 0;
     }
-
+    // Create append function to add a new node to the end of the list
     append(value) {
         const newNode = new Node(value);
         if (this.size === 0) {
@@ -24,7 +25,7 @@ class LinkedList {
         this.size++;
         this.displayOperation('append', value);
     }
-
+    // Create propend function to add a new node to the start of the list
     prepend(value) {
         const newNode = new Node(value);
         if (this.size === 0) {
@@ -37,22 +38,22 @@ class LinkedList {
         this.size++;
         this.displayOperation('prepend', value);
     }
-
+    // Create size function to get the total number of nodes in the list
     getSize() {
         this.displayOperation('size', this.size);
         return this.size;
     }
-
+    // Create head function to get the first node
     getHead() {
         this.displayOperation('head', this.head ? this.head.value : null);
         return this.head;
     }
-
+    // Create tail function to get the last node
     getTail() {
         this.displayOperation('tail', this.tail ? this.tail.value : null);
         return this.tail;
     }
-
+    // Create at function to get a node at a given index
     at(index) {
         if (index < 0 || index >= this.size) {
             this.displayOperation('at', `Index ${index} out of range`);
@@ -65,7 +66,7 @@ class LinkedList {
         this.displayOperation('at', currentNode.value);
         return currentNode;
     }
-
+    // Create pop function to remove the last node from the list
     pop() {
         if (this.size === 0) {
             this.displayOperation('pop', 'List is empty');
@@ -87,7 +88,7 @@ class LinkedList {
         this.displayOperation('pop', deletedNode.value);
         return deletedNode;
     }
-
+    // Create contains function to return check if a parsed in value is in the list
     contains(value) {
         let currentNode = this.head;
         while (currentNode !== null) {
@@ -100,7 +101,7 @@ class LinkedList {
         this.displayOperation('contains', false);
         return false;
     }
-
+    // Create find function to search list values
     find(value) {
         let currentNode = this.head;
         let index = 0;
@@ -115,7 +116,6 @@ class LinkedList {
         this.displayOperation('find', null);
         return null;
     }
-
     // Function to represent the linked list as a string
     toString() {
         let result = '';
@@ -127,7 +127,6 @@ class LinkedList {
         result += 'null';
         return result;
     }
-
     // Function to display the operation and its result
     displayOperation(operation, result) {
         console.log(`${operation}:`, result);
@@ -135,8 +134,7 @@ class LinkedList {
         console.log('-------------------');
     }
 }
-
-// Example usage
+// Examples to check if everything is ok
 const linkedList = new LinkedList();
 linkedList.append(1);
 linkedList.append(2);
